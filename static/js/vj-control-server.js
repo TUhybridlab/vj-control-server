7851224cf6c2ab7509a761fa37ec90956fd352bd
Juri Berlanda <juriberlanda@hotmail.com>
2015-03-16 02:02:43 +0100

    [FanControl] Refactored.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- static/js/vj-control-server.js
+++ static/js/vj-control-server.js
@@ -1,4 +1,10 @@
-ajax = function(uri, method, data, is_async, sucess, error) {
+do_nothing = function() {}
+default_error = function (jqXHR) {
+			console.log("ajax error " + jqXHR.status);
+			serverDown = true;
+}
+
+ajax = function(uri, method, request_data, is_async, success_callback, error_callback) {
 	var request = {
 		url: uri,
 		type: method,
@@ -7,27 +13,56 @@ ajax = function(uri, method, data, is_async, sucess, error) {
 		cache: false,
 		dataType: 'json',
 		async: is_async,
-		data: JSON.stringify(data),
-		success: function() {
+		success: function(data) {
 			serverDown = false;
+			success_callback(data);
 		},
 		error: function(jqXHR) {
-			console.log("ajax error " + jqXHR.status);
-			serverDown = true;
+			default_error();
+			error_callback();
 		}
 	};
+
+	if (request_data != null)
+		request.data = JSON.stringify(request_data);
+
 	return $.ajax(request);
 }
 
-setFanSpeed = function(speed_percentage) {
-	ajax("/fan/" + speed_percentage, "PUT", null, true, setFanSpeed_success, default_error);
+FanSlider = function() {
+	var self = this;
+	var self.onChangeCallbackEnabled = true;
+
+	self.initSlider = function () {
+		ajax("/fan/", "GET", null, false, do_nothing, do_nothing).done(function(data) {
+			// Initilaize slider with value
+			$( "#slider" ).slider({
+				value: data.speed
+			});
+
+			// Set speed with slider
+			$( "#slider" ).on( "slidechange", function( event, ui ) {
+				if (onChangeCallbackEnabled)
+					setFanSpeed($( "#slider" ).slider( "value" ));
+			});
+		});
+	}
+
+	self.setSlider = function(data) {
+		console.log("setting slider to " + data.speed);
+		onChangeCallbackEnabled = false;
+		$( "#slider" ).slider( "option", "value", data.speed);
+		onChangeCallbackEnabled = true;
+	}
+
+	self.setFanSpeed = function(speed_percentage) {
+		ajax("/fan/" + speed_percentage, "PUT", null, true, do_nothing, do_nothing);
+	}
+
+	getFanSpeed = function() {
+		ajax("/fan/", "GET", null, false, do_nothing, do_nothing).done(function(data) { FanSlider.setSlider(data); });
+	}
+
 }
 
-setFanSpeed_success = function() {
-	alert("TODO")
-}
-
-default_error = function (jqXHR) {
-	console.log("ajax error " + jqXHR.status);
-	serverDown = true;
-}
+FanSlider.initSlider();
