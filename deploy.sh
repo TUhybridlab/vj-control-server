65f91e2593baa8f84bd3763b098c4e0888b098dd
Juri Berlanda <juriberlanda@hotmail.com>
2015-03-22 15:04:05 +0100

    [Scripts] Reworked deploy to work on different IPs.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-ssh pi@192.168.1.50 "mkdir -p vj-control-server"
-scp -r * pi@192.168.1.50:~/vj-control-server
-ssh pi@192.168.1.50 "chmod +x vj-control-server/launch.sh"
+IP=192.168.1.50
+
+if [ -z $PI_IP ]
+then
+	IP=$PI_IP
+fi
+
+ssh pi@$PI_IP "mkdir -p vj-control-server"
+scp -r * pi@${PI_IP}:~/vj-control-server
+ssh pi@$PI_IP "chmod +x vj-control-server/launch.sh"
